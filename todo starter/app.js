f3958038aa2acb7914717e221cb31c6ffdac02ac
sehabiissam <sehabiissm8@gmail.com>
2026-03-18 10:33:00 +0100

    first commit

---
+++ todo starter/app.js
@@ -0,0 +1,15 @@
+let tasks = [];
+
+function ddTask() {
+    const task = document.querySelector('.js-task-input').value;
+
+    
+
+    tasks.push({
+        name: task,
+        completed: false
+    });
+
+    render-
+
+}
